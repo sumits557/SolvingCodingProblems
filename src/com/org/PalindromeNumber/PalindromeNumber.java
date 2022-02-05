d2d25eca04b4737be710c16ef6586891a6ee4db2
sumits557 <sumits557@gmail.com>
2022-02-05 17:11:41 -0600

    Added palindrome number or not

---
+++ src/com/org/PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,22 @@
+public class ReverseInteger {
+
+        /**
+         * GGiven an integer x, return true if x is palindrome integer.
+         *
+         * An integer is a palindrome when it reads the same backward as forward.
+         *
+         * For example, 121 is a palindrome while 123 is not.
+         */
+        public boolean isPalindrome(int x) {
+            String str = String.valueOf(x);
+            int start = 0;
+            int end = str.length() - 1;
+            while(start < end){
+                if(str.charAt(start++) != str.charAt(end--)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+}
