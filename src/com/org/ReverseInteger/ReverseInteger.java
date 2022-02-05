42834ac50b50f056f42f082caa0b97420ec2e900
sumits557 <sumits557@gmail.com>
2022-02-05 16:56:56 -0600

    Added reverse integer logic

---
+++ src/com/org/ReverseInteger/ReverseInteger.java
@@ -0,0 +1,26 @@
+public class ReverseInteger {
+
+    /**
+     * Given a signed 32-bit integer x, return x with its digits reversed.
+     * If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0
+     *
+     * @param x int
+     * @return rev int
+     */
+    public int reverse(int x) {
+
+        int rev = 0;
+        while(x != 0) {
+            int pop = x % 10; x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+}
