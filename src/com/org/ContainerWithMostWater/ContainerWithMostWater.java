83fb39665f3bde2b3f8c90b1cf2cfa125575930a
sumits557 <sumits557@gmail.com>
2022-02-06 19:22:14 -0600

    Added Container with most water.

--- src/com/org/ContainerWithMostWater/ContainerWithMostWater.java
+++ src/com/org/ContainerWithMostWater/ContainerWithMostWater.java
@@ -9,9 +9,11 @@ public class ContainerWithMostWater {
      */
     public int maxArea(int[] height) {
             int maxarea = 0;
-            for (int i = 0; i < height.length; i++)
-                for (int j = i + 1; j < height.length; j++)
-                    maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
+            for (int i = 0; i < height.length; i++) {
+                for (int j = i+1; j < height.length; j++) {
+                    maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j-i))
+                }
+            }
             return maxarea;
         }
 
